33fb11a8e0549c7d079061980e810f440510da1d
Ehren Dames <ehren@ehrendames.com>
2016-04-02 12:10:42 -0400

    feat(Albums): add Albums View and Layout
    Add AngularJS reference on Layout
    Add View for Albums
    Add pre-build copy for AngularJS files

---
+++ MusicStore.Web/Controllers/AlbumController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Web.Controllers
+{
+    public class AlbumController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View(Views.Index);
+        }
+
+        public static class Views
+        {
+            public static readonly string Index = "/Views/Albums/View.cshtml";
+        }
+    }
+}
