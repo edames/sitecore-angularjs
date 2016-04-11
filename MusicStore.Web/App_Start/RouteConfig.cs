fe739287f19ffbdc269fdc3ec592ca457548d1e7
Ehren Dames <ehren@ehrendames.com>
2016-04-10 20:18:05 -0400

    fix(Routing): albums route should come first

--- MusicStore.Web/App_Start/RouteConfig.cs
+++ MusicStore.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MusicStore.Web
@@ -12,6 +8,12 @@ namespace MusicStore.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Albums",
+                url: "albums/index",
+                defaults: new { controller = "Albums", action = "Index" }
+            );
 
             routes.MapRoute(
                 name: "Default",
